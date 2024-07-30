8e9be0911dc1c5c5b6b339ffa138333418556b13
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2024-07-30 06:09:54 -1100

    Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 const Error = () => {
   return (
-    <div>
+    <section className="pages">
       <h2>404 Error Message</h2>
       <p>Page not found</p>
       <Link to={"/"}>back to home</Link>
-    </div>
+    </section>
   );
 };
 
