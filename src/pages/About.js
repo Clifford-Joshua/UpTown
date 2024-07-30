4d0aa8a18540431a91ab47e5cbd9faa6ed758c5e
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2024-07-30 12:36:35 -1100

    Aboutpage

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,34 @@
 import React from "react";
-
+import AboutImg from "../Images/Aboul-Bg.png";
+import { Link } from "react-router-dom";
 const About = () => {
   return (
     <section className="pages">
-      <h2>About Us</h2>
+      {/* ==pageSetUp=== */}
+      <div className="pagesetup two-div">
+        <div className="about-bg">
+          <img src={AboutImg} alt="About-bg" />
+        </div>
+        <div className="coloum-set">
+          <div>
+            <h2 className="new-font">About</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe
+              voluptate velit animi autem et quas dignissimos soluta facilis
+              asperiores tempore? Facere eligendi, rerum quam cupiditate
+              deserunt sint, culpa illo accusantium earum fuga amet ratione
+            </p>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+              enim cum in recusandae accusamus perspiciatis nemo assumenda
+            </p>
+            <Link className="links">Our Gallery</Link>
+          </div>
+          <p></p>
+        </div>
+      </div>
     </section>
   );
 };
