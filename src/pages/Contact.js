09d2eb830977ef6df173da0b071057be0c61e900
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2024-07-31 02:34:45 -1100

    highrisepage

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <section className="pages">
-      <h2>Contact Us</h2>
+      <h2>Contact</h2>
     </section>
   );
 };
--- src/pages/Highrise.js
+++ src/pages/Highrise.js
@@ -1,9 +1,50 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Highrise = () => {
   return (
-    <section className="pages">
-      <h2>High Rise</h2>
+    <section className=" pages">
+      <div className="pagesetup coloum-set full-width highrise">
+        <ul className="high-rise-link">
+          <li>
+            <Link className="high-link">Signature</Link>
+          </li>
+          <li>
+            <Link className="high-link">METROPOLIAN</Link>
+          </li>
+          <li>
+            <Link className="high-link">MUSE</Link>
+          </li>
+          <li>
+            <Link className="high-link">ICON</Link>
+          </li>
+          <li>
+            <Link className="high-link">STARGAZER</Link>
+          </li>
+          <li>
+            <Link className="high-link">HISERISE</Link>
+          </li>
+          <li>
+            <Link className="high-link">LOWRISE</Link>
+          </li>
+        </ul>
+        <div>
+          <h2 className="new-font">HIGH-RISE</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates
+            quidem eligendi neque dolore repudiandae, est iusto consequuntur
+            eius expedita adipisci. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Alias quam atque vitae maxime eaque debitis cumque
+            quidem eos voluptates suscipit in consequatur officiis hic cum totam
+            repudiandae voluptatibus pariatur repellendus tempora, iusto, nulla,
+            doloremque quod. Iusto earum illum nostrum est?Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Magni animi temporibus alias
+            debitis hic deserunt aut sint inventore incidunt eaque et magnam,
+            vel iure libero eius doloribus eos beatae. Velit?Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Laboriosam
+          </p>
+          <button className="btn-payment">$7</button>
+        </div>
+      </div>
     </section>
   );
 };
